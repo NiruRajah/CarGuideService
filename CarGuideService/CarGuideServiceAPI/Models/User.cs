94c00b7d5c78261b7602a474c96621f8cc6636a1
Nirusan <shanrajah@hotmail.com>
2020-11-22 00:39:58 -0500

    Fixed order of files

---
+++ CarGuideService/CarGuideServiceAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarGuideServiceAPI.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //public List<Vehicle> ListOfVehicles { get; set; }
+    }
+}
