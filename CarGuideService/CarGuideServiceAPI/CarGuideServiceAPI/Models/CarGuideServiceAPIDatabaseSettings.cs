99807e10bdb42d9bbb44e07637160fbbdcedc5a1
Nirusan Nadarajah <shanrajah@hotmail.com>
2020-11-22 00:36:42 -0500

    Added More RESTAPI calls

---
+++ CarGuideService/CarGuideServiceAPI/CarGuideServiceAPI/Models/CarGuideServiceAPIDatabaseSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarGuideServiceAPI.Models
+{
+    public class CarGuideServiceAPIDatabaseSettings : ICarGuideServiceAPIDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ICarGuideServiceAPIDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
